4002dd46e5f3d5e7eeed3758d5e0d23e2ac0c541
Gabriel Ribeiro <gutorico29@gmail.com>
2025-05-07 16:24:36 -0400

    projeto completo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -85,25 +85,32 @@ export default function Home() {
             todos.length > 0 ? todos.map((todo) => {
               return (
                 <div className={styles.todocard}>
-                  <h2>{todo.text}</h2>
-                  <svg
-                    onClick={() => {
-                      deleteTodo(todo.id)
-                    }}
+                  {
+                    todo.status == false ?
+                    <h2 className={styles.taskpending}>{todo.text}</h2>
+                    :
+                    <h2 className={styles.taskdone}>{todo.text}</h2>
+                  }
+                  <div className={styles.icons}>
+                    <svg
+                      onClick={() => {
+                        deleteTodo(todo.id)
+                      }}
 
-                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
-                  </svg>
+                      xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
+                    </svg>
 
 
-                  <svg
-                    onClick={() => {
-                      updateTodo(todo.id)
-                    }}
+                    <svg
+                      onClick={() => {
+                        updateTodo(todo.id)
+                      }}
 
-                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                  </svg>
+                      xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                    </svg>
+                  </div>
 
 
                 </div>
@@ -116,13 +123,18 @@ export default function Home() {
           }
         </div>
         <button
+          className={styles.openModalBtn}
           onClick={() => {
             setShowModal(true)
           }}
-        >Create Todo</button>
-        {
-          showModal &&
-          <div className={styles.createTodoModal}>
+        >+</button>
+
+      </div>
+
+      {
+        showModal &&
+        <div className={styles.createTodoModal}>
+          <div className={styles.createTodoModalIn}>
             <input type="text" placeholder="Todo Text"
               value={newTodoText} onChange={(e) => { setNewTodoText(e.target.value) }}
             />
@@ -133,8 +145,10 @@ export default function Home() {
               }}
             >+</button>
           </div>
-        }
-      </div></div>
+        </div>
+      }
+    </div>
+
 
 
   );
